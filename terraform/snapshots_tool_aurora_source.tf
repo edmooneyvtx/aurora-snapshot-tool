9bdcd7ffefac3113f44774e36abd6e2f11f8c6ca
Ed Mooney <33490546+edmooneyvtx@users.noreply.github.com>
2020-01-31 15:47:32 -0500

    Update snapshots_tool_aurora_source.tf

--- terraform/snapshots_tool_aurora_source.tf
+++ terraform/snapshots_tool_aurora_source.tf
@@ -233,14 +233,107 @@ resource "aws_cloudwatch_metric_alarm" "delete_old_failed" {
 	}
 
           
-          
+ /*         
           "lambdaCopySnapshotsAurora": {
 			"Type": "AWS::Lambda::Function",
             
+
+*/
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "CopySnapshotsAurora"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+
+  runtime = "nodejs8.10"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+
+/*
             
-            "lambdaDeleteOldDestAurora": {
-			"Type": "AWS::Lambda::Function",
-			"Condition": "DeleteOld",
+"lambdaDeleteOldDestAurora": {
+	"Type": "AWS::Lambda::Function",
+	"Condition": "DeleteOld",
+*/
+					
+					
+					
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "DeleteOldDestAurora"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+
+  runtime = "nodejs8.10"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+
  
 /*
               
