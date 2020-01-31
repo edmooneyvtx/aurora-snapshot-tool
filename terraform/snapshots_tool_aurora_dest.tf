f49d3bf03b1558b558730b458668587e7a7329b0
Ed Mooney <33490546+edmooneyvtx@users.noreply.github.com>
2020-01-31 10:41:57 -0500

    Update snapshots_tool_aurora_dest.tf

--- terraform/snapshots_tool_aurora_dest.tf
+++ terraform/snapshots_tool_aurora_dest.tf
@@ -1,1 +1,36 @@
 #
+
+
+Terraform needed:
+
+SNS snapshot topic: name: topic_aurora_tool_dest": "Type": "AWS::SNS::Topic",
+- policy_doc for SNS: "Type": "AWS::SNS::TopicPolicy",
+
+"Type": "AWS::CloudWatch::Alarm",
+
+		"alarmcwCopyFailedDest": {
+			"Type": "AWS::CloudWatch::Alarm",
+
+alarmcwDeleteOldFailedDest": {
+			"Type": "AWS::CloudWatch::Alarm",
+
+		"iamroleSnapshotsAurora": {
+			"Type": "AWS::IAM::Role",
+
+		"lambdaCopySnapshotsAurora": {
+			"Type": "AWS::Lambda::Function",
+
+            		"lambdaDeleteOldDestAurora": {
+			"Type": "AWS::Lambda::Function",
+
+
+		"iamroleStateExecution": {
+			"Type": "AWS::IAM::Role",
+
+
+		"statemachineCopySnapshotsDestAurora": {
+			"Type": "AWS::StepFunctions::StateMachine",
+
+            		"statemachineDeleteOldSnapshotsDestAurora": {
+			"Type": "AWS::StepFunctions::StateMachine",
+			"Condition": "DeleteOld",
