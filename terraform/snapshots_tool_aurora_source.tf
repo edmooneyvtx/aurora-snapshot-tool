53531e809430448a04192595d2b48f189d1c9b71
Ed Mooney <33490546+edmooneyvtx@users.noreply.github.com>
2020-02-03 07:45:00 -0500

    Update snapshots_tool_aurora_source.tf

--- terraform/snapshots_tool_aurora_source.tf
+++ terraform/snapshots_tool_aurora_source.tf
@@ -9,6 +9,14 @@
 # tie configurations together
 # SNS - config
 
+/*
+	
+
+
+	
+
+*/
+
 
 /*
 	  tags = {
@@ -252,15 +260,15 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  filename      = "${LambdaFunctionName}.zip"
+  function_name = "${LambdaFunctionName}"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "exports.test"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+  source_code_hash = "${filebase64sha256("${LambdaFunctionName}.zip")}"
 
   runtime = "nodejs8.10"
 
@@ -303,6 +311,10 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+# delete old - really this is a refresh
+	
+/*
+		
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
@@ -322,7 +334,8 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 }
-
+	
+*/
 
 
  
